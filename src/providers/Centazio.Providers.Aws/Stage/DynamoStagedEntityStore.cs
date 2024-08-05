846b331ab7192855fdbdb8ec2e70d044c64e7b6f
Guido Tapia <guido.tapia@picnet.com.au>
2024-08-05 19:51:54 +1000

    experimenting with the concept of providers

---
+++ src/providers/Centazio.Providers.Aws/Stage/DynamoStagedEntityStore.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
+using Centazio.Core;
+using Centazio.Core.Entities.Ctl;
+using Centazio.Core.Stage;
+
+namespace Centazio.Providers.Aws.Stage;
+
+public class DynamoStagedEntityStore : AbstractStagedEntityStore, IDisposable {
+
+  private readonly IAmazonDynamoDB client;
+
+  public DynamoStagedEntityStore() {
+    var (key, secret) = ("", "");
+    client = new AmazonDynamoDBClient(
+        new BasicAWSCredentials(key, secret), 
+        new AmazonDynamoDBConfig { RegionEndpoint = RegionEndpoint.APSoutheast2 });
+  }
+
+  public void Dispose() { client.Dispose(); }
+
+  public async Task Initalise() {
+    var tables = await client.ListTablesAsync();
+    Console.WriteLine("Tables: " + JsonSerializer.Serialize(tables, new JsonSerializerOptions { WriteIndented = true }));
+    // var desc = await client.DescribeTableAsync("staged_entity");
+    // Console.WriteLine(JsonSerializer.Serialize(desc, new JsonSerializerOptions { WriteIndented = true }));
+  }
+
+  public override Task Save(DateTime stageddt, SystemName source, ObjectName obj, string data) => throw new NotImplementedException();
+  public override Task Save(DateTime stageddt, SystemName source, ObjectName obj, IEnumerable<string> datas) => throw new NotImplementedException();
+  protected override Task<IEnumerable<StagedEntity>> GetImpl(DateTime since, SystemName source, ObjectName obj) => throw new NotImplementedException();
+
+}
---
+++ tests/Centazio.Providers.Aws.Tests/Stage/DynamoStagedEntityStoreTests.cs
@@ -0,0 +1,11 @@
+﻿using Centazio.Providers.Aws.Stage;
+
+namespace Centazio.Providers.Aws.Tests.Stage;
+
+public class DynamoStagedEntityStoreTests {
+  [Test] public async Task Go() {
+    var dynamo = new DynamoStagedEntityStore();
+    await dynamo.Initalise();
+    Console.WriteLine("Done");
+  }
+}
