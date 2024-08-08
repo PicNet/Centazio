61403eba6d8fb7b3b1ab1d1e64135bd7e9d64e2b
Guido Tapia <guido.tapia@picnet.com.au>
2024-08-08 10:01:29 +1000

    Initial tested implementation of strongly typed SettingsLoader

--- src/Centazio.Core/Settings/SettingsLoader.cs
+++ src/Centazio.Core/Settings/SettingsLoader.cs
@@ -1,23 +1,27 @@
-﻿using System.Text.Json;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace Centazio.Core.Settings;
 
 public interface ISettingsLoader<T> {
-  T Load();
+  T Load(string environment = "");
 }
 
-public class SettingsLoader<T>(string filename = SettingsLoader<T>.DEFAULT_FILE_NAME) : ISettingsLoader<T> {
+public class SettingsLoader<T>(string filename = SettingsLoader<T>.DEFAULT_FILE_NAME) : ISettingsLoader<T> where T : new() {
 
   private const string DEFAULT_FILE_NAME = "settings.json";
   
-  public T Load(string environment) {
+  public T Load(string environment = "") {
     if (!filename.EndsWith(".json")) throw new Exception("settings file should have a json extension");
+    
     var basefile = SearchForSettingsFile(filename) ?? throw new Exception($"could not find settings file [{filename}] in the current directory hierarchy");
-    var obj = JsonSerializer.Deserialize<T>(File.ReadAllText(basefile)) ?? throw new Exception();
-    var envfile =  SearchForSettingsFile(filename.Replace(".json", $".{environment}.json"));
-    if (envfile != null) {
-      var envobj = JsonSerializer.Deserialize<T>(File.ReadAllText(envfile)) ?? throw new Exception();
-    }
+    var builder = new ConfigurationBuilder().AddJsonFile(basefile, false);
+    var envfile = String.IsNullOrEmpty(environment) ? 
+        null : 
+        SearchForSettingsFile(filename.Replace(".json", $".{environment}.json"));
+    if (envfile != null) { builder.AddJsonFile(envfile, false); }
+    
+    var obj = new T();
+    builder.Build().Bind(obj);
     return obj;
   }
 
--- tests/Centazio.Core.Tests/Settings/SettingsLoaderTests.cs
+++ tests/Centazio.Core.Tests/Settings/SettingsLoaderTests.cs
@@ -3,10 +3,40 @@
 namespace centazio.core.tests.Settings;
 
 public class SettingsLoaderTests {
-  [Test] public void Test_loading_of_settings() {
-    var settings = new SettingsLoader<TestSettingsObj>("test_settings.json").Load();
-    Assert.That(settings.FileForTestingSettingsLoader, Is.EqualTo("Do not modify"));
+
+  private static readonly string test_settings_json = @"{ ""FileForTestingSettingsLoader"": ""Testing content"", ""OverridableSetting"": ""To be overriden"", ""EmptySetting"": """", ""MissingSetting"": null }";
+  private static readonly string test_settings_test_json = @"{ ""OverridableSetting"": ""Overriden"", ""EmptySetting"": ""No longer empty"", ""MissingSetting"": ""No longer missing"" }";
+  
+  [TearDown] public void TearDown() {}
+  
+  [Test] public void Test_loading_of_settings_from_dir_hierarchy() {
+    TestSettings(CreateLoadAndDeleteSettings("."));
+    TestSettings(CreateLoadAndDeleteSettings(".."));
+    TestSettings(CreateLoadAndDeleteSettings("../.."));
+    
+    void TestSettings(TestSettingsObj loaded) => Assert.That(loaded, Is.EqualTo(new TestSettingsObj("Testing content", "To be overriden", "", "")));
+  }
+  
+  [Test] public void Test_loading_of_settings_with_overriding_environment_file() {
+    TestSettings(CreateLoadAndDeleteSettings(".", "test"));
+    TestSettings(CreateLoadAndDeleteSettings("..", "test"));
+    TestSettings(CreateLoadAndDeleteSettings("../..", "test"));
+                
+    void TestSettings(TestSettingsObj loaded) => Assert.That(loaded, Is.EqualTo(new TestSettingsObj("Testing content", "Overriden", "No longer empty", "No longer missing")));
+  }
+  
+  private TestSettingsObj CreateLoadAndDeleteSettings(string dir, string environment="") {
+    try {
+      File.WriteAllText(Path.Combine(dir, "test_settings.json"), test_settings_json);
+      File.WriteAllText(Path.Combine(dir, $"test_settings.{environment}.json"), test_settings_test_json);
+      return new SettingsLoader<TestSettingsObj>("test_settings.json").Load(environment); 
+    } finally { 
+      File.Delete(Path.Combine(dir, "test_settings.json"));
+      File.Delete(Path.Combine(dir, $"test_settings.{environment}.json"));
+    }
   }
 }
 
-public record TestSettingsObj(string FileForTestingSettingsLoader);
+public record TestSettingsObj(string FileForTestingSettingsLoader, string OverridableSetting, string EmptySetting, string MissingSetting) {
+  public TestSettingsObj() : this("", "", "", "") {}
+}
