f69acdbbac2cbd2d22957983fc1d6652fcf655e3
Guido Tapia <guido.tapia@picnet.com.au>
2025-03-21 11:15:19 +1100

    starting work on centazio code generation

---
+++ defaults/templates/centazio/function.cs
@@ -0,0 +1,26 @@
+using Centazio.Core.Misc;
+using Centazio.Core.Runner;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace {{it.AssemblyName}}.{{it.ClassName}};
+
+public class {{it.ClassName}} : {{it.FunctionType}} {
+  private static readonly CentazioServicesRegistrar registrar = new(new ServiceCollection());
+  private static readonly Lazy<Task<IRunnableFunction>> impl;
+
+  static {{it.ClassName}}Azure() {    
+    impl = new(async () => (await new FunctionsInitialiser({{it.Environments}}, registrar)
+        .Init([typeof({{it.ClassName}})])).Single(), LazyThreadSafetyMode.ExecutionAndPublication);
+  }
+
+  [Function(nameof({{it.ClassName}}))] public async Task Run([TimerTrigger("{{ it.FunctionTimerCronExpr }}")] TimerInfo timer) {    
+    var start = UtcDate.UtcNow;
+    log.LogInformation("{{it.ClassName}} running");
+    try { 
+      var (function, runner) = (await impl.Value, registrar.ServiceProvider.GetRequiredService<IFunctionRunner>());
+      await runner.RunFunction(function); 
+    } finally { log.LogInformation($"{{it.ClassName}} completed, took {(UtcDate.UtcNow - start).TotalSeconds:N0}s"); }
+  }
+}
--- src/Centazio.Cli/Commands/Gen/Centazio/GenerateFunctionCommand.cs
+++ src/Centazio.Cli/Commands/Gen/Centazio/GenerateFunctionCommand.cs
@@ -5,7 +5,7 @@ using Spectre.Console.Cli;
 
 namespace Centazio.Cli.Commands.Gen.Centazio;
 
-public class GenerateFunctionCommand(ITemplater templater) : AbstractCentazioCommand<GenerateFunctionCommand.Settings> {
+public class GenerateFunctionCommand(ICommandRunner cmd, ITemplater templater) : AbstractCentazioCommand<GenerateFunctionCommand.Settings> {
 
   protected override Task<Settings> GetInteractiveSettings() => Task.FromResult(new Settings { 
     FunctionName = UiHelpers.Ask("Function Name")
@@ -18,6 +18,8 @@ public class GenerateFunctionCommand(ITemplater templater) : AbstractCentazioCom
     }
     
     var dir = Directory.CreateDirectory(settings.FunctionName);
+    cmd.DotNet($"sln add {settings.FunctionName}", dir.FullName);
+    
     var contents = templater.ParseFromPath("Solution.sln", new { Projects = 1 });
     Console.WriteLine(contents);
     return Task.CompletedTask;
@@ -25,6 +27,10 @@ public class GenerateFunctionCommand(ITemplater templater) : AbstractCentazioCom
 
   public class Settings : CommonSettings {
     [CommandArgument(0, "<FUNCTION_NAME>")] public required string FunctionName { get; init; }
+    [CommandOption("-r|--read")] public bool Read { get; set; }
+    [CommandOption("-p|--promote")] public bool Promote { get; set; }
+    [CommandOption("-w|--write")] public bool Write { get; set; }
+    [CommandOption("-o|--other")] public bool Other { get; set; }
   }
 
 }
