67a01d5d7f65425ae48d352a77c7deeaa80861aa
William To <william.to@picnet.com.au>
2025-05-29 00:57:11 +1000

    Added secretsloader to provider host

--- src/Centazio.Hosts/Centazio.Hosts.Aws/AwsHostCentazioEngineAdapter.cs
+++ src/Centazio.Hosts/Centazio.Hosts.Aws/AwsHostCentazioEngineAdapter.cs
@@ -1,17 +1,25 @@
-﻿using Centazio.Core.Ctl;
+﻿using Centazio.Core;
+using Centazio.Core.Ctl;
 using Centazio.Core.Engine;
 using Centazio.Core.Misc;
 using Centazio.Core.Runner;
+using Centazio.Core.Secrets;
 using Centazio.Core.Settings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Centazio.Hosts.Aws;
 
 public class AwsHostCentazioEngineAdapter(CentazioSettings settings, List<string> environments, bool localaws) : CentazioEngine(environments) {
+  private List<string> environments = environments;
   protected override void RegisterHostSpecificServices(CentazioServicesRegistrar registrar) {
     // todo: this should support function-to-function triggers
     var notifier = new AwsSqsChangesNotifier(localaws);
-        
+    var factory = registrar.BuildServiceProvider().GetRequiredService<IServiceFactory<ISecretsLoader>>();
+    var loader = factory.GetService();
+    var secrets = loader.Load<CentazioSecrets>(environments).Result;
+    
+    registrar.Register(secrets);
+    registrar.Register<ISecretsLoader>(_ => loader);
     registrar.Register<IChangesNotifier>(notifier);
     registrar.Register<IFunctionRunner>(prov => {
       var inner = new FunctionRunner(prov.GetRequiredService<ICtlRepository>(), settings);
--- src/Centazio.Hosts/Centazio.Hosts.Az/AzHostCentazioEngineAdapter.cs
+++ src/Centazio.Hosts/Centazio.Hosts.Az/AzHostCentazioEngineAdapter.cs
@@ -1,13 +1,24 @@
-﻿using Centazio.Core.Engine;
+﻿using Centazio.Core;
+using Centazio.Core.Engine;
 using Centazio.Core.Misc;
 using Centazio.Core.Runner;
+using Centazio.Core.Secrets;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Centazio.Hosts.Az;
 
 public class AzHostCentazioEngineAdapter(List<string> environments) : CentazioEngine(environments) {
 
+  private List<string> environments = environments;
   protected override void RegisterHostSpecificServices(CentazioServicesRegistrar registrar) {
     // todo: this should support function-to-function triggers
+    var factory = registrar.BuildServiceProvider().GetRequiredService<IServiceFactory<ISecretsLoader>>();
+    var loader = factory.GetService();
+    var secrets = loader.Load<CentazioSecrets>(environments).Result;
+        
+    // Register services
+    registrar.Register(secrets);
+    registrar.Register<ISecretsLoader>(_ => loader);
     registrar.Register<IFunctionRunner, FunctionRunner>();
   }
 
